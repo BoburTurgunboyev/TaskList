3ac217c3b6707f2dff66a29c6d222cd4e73a3057
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-02-16 02:05:04 +0500

    entities Added

---
+++ src/TaskList.Domain/Entities/Task.cs
@@ -0,0 +1,12 @@
+﻿using TaskList.Domain.Enums;
+
+namespace TaskList.Domain.Entities;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public Status Status { get; set; }
+}

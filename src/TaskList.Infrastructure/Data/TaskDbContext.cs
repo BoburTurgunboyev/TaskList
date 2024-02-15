18f56fc971834ecac425f5268cfec083a0305b92
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-02-16 02:04:38 +0500

    data added

---
+++ src/TaskList.Infrastructure/Data/TaskDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TaskList.Domain.Entities;
+using Task = TaskList.Domain.Entities.Task;
+
+namespace TaskList.Infrastructure.Data
+{
+    public class TaskDbContext : IdentityDbContext<User,Role,Guid>
+    {
+        private readonly IConfiguration _configuration;
+        public TaskDbContext(DbContextOptions<TaskDbContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+            Database.Migrate();
+        }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+        }
+
+
+    }
+}

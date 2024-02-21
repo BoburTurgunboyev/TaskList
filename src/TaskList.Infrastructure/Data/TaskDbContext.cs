7e8320fec72ca16a3b0915946c69574ef7e37ddc
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-02-22 03:48:41 +0500

    db changed

--- src/TaskList.Infrastructure/Data/TaskDbContext.cs
+++ src/TaskList.Infrastructure/Data/TaskDbContext.cs
@@ -16,12 +16,30 @@ namespace TaskList.Infrastructure.Data
             Database.Migrate();
         }
         public DbSet<Task> Tasks { get; set; }
-        public DbSet<User> Users { get; set; }
+       
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            SeedRoles(builder);
+        } 
+
+
+        private static void SeedRoles(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityRole>()
+                .HasData(
+                new IdentityRole() { Name = "Admin", ConcurrencyStamp = "1", NormalizedName = "Admin" },
+                new IdentityRole() { Name = "Manager", ConcurrencyStamp = "3", NormalizedName = "Manager" },
+                new IdentityRole() { Name = "User", ConcurrencyStamp = "2", NormalizedName = "User" }
+
+                );
         }
 
 

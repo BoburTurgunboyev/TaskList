dfe411bf904f42d94280da4a70338edc7e76bc1c
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-02-22 03:49:00 +0500

    service added

---
+++ src/TaskList.Application/Services/AccountServ/AccountService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TaskList.Domain.Dtos;
+using TaskList.Domain.Entities;
+
+
+
+namespace TaskList.Application.Services.AccountServ
+{
+    public class AccountService : IAccountService
+    {  
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
+
+        public ValueTask<bool> Login(LoginDto loginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<bool> Registor(RegistorDto registorDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/TaskList.Application/Services/AccountServ/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskList.Domain.Dtos;
+
+namespace TaskList.Application.Services.AccountServ
+{
+    public interface IAccountService
+    {
+        public ValueTask<bool> Login(LoginDto loginDto);
+        public ValueTask<bool> Registor(RegistorDto registorDto);
+    }
+}
